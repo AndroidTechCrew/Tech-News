ee0d51c0a33b50d8beb092557b6d8e87e4ec083d
NelsonTejeda <nelsonjesustejeda@gmail.com>
2021-04-17 01:13:49 -0400

    added profile content with the code Chris provided

--- app/src/main/java/com/AndroidTechCrew/technews/fragments/ProfileFragment.java
+++ app/src/main/java/com/AndroidTechCrew/technews/fragments/ProfileFragment.java
@@ -7,18 +7,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.AndroidTechCrew.technews.LoginActivity;
 import com.AndroidTechCrew.technews.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProfileFragment extends Fragment {
+    private static final String TAG = "ProfileFragment";
     private Button buttonLogout;
     private FirebaseAuth mAuth;
+    private TextView username;
+    private ImageView profileImage;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     public ProfileFragment() {
@@ -37,6 +52,21 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
+        username = view.findViewById(R.id.tvProfileUsername);
+        profileImage = view.findViewById(R.id.ivProfileImage);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        db.collection("users").document(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Document found in the offline cache
+                    DocumentSnapshot document = task.getResult();
+                    username.setText(document.getData().get("email").toString());
+                } else {
+                    Log.d(TAG, "Cached get failed: ", task.getException());
+                }
+            }
+        });
         buttonLogout = view.findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
