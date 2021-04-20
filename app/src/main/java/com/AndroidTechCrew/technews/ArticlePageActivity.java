18478bc1ef55c9f08de4e9cdc747c699335b141f
NelsonTejeda <nelsonjesustejeda@gmail.com>
2021-04-19 21:15:03 -0400

    cleaning more code

--- app/src/main/java/com/AndroidTechCrew/technews/ArticlePageActivity.java
+++ app/src/main/java/com/AndroidTechCrew/technews/ArticlePageActivity.java
@@ -3,12 +3,20 @@ package com.AndroidTechCrew.technews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ArticlePageActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_page);
+        String link = getIntent().getStringExtra("link");
+        webView = (WebView) findViewById(R.id.wvArticlePage);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(link);
     }
 }
